f50cde917f56423827871a1ebb1d03988c85e21a
Alejandro <8r5mfbht6q@privaterelay.appleid.com>
2026-08-16 21:47:58 -0500

    nuevo proyecto

---
+++ src/main/java/com/mericar/backend_mericar/BackendMericarApplication.java
@@ -0,0 +1,13 @@
+package com.mericar.backend_mericar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendMericarApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BackendMericarApplication.class, args);
+	}
+
+}
